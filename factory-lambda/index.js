5c7698df1e2db89c8e8f95e53e0381aeb450ae29
glen15 <glen910115@gmail.com>
2023-05-18 15:47:52 +0900

    update factory lambda for sns publish

--- factory-lambda/index.js
+++ factory-lambda/index.js
@@ -3,6 +3,9 @@ const serverless = require("serverless-http");
 require('dotenv').config()
 const express = require("express");
 
+const AWS = require("aws-sdk")
+const sns = new AWS.SNS({ region: "ap-northeast-2" })
+
 const app = express();
 app.use(express.json());
 
@@ -35,31 +38,34 @@ app.post("/log", connectDb, async (req, res, next) => {
   console.log("생산 요청자: ", requester)
   console.log("생산 완료 후 item db quantity 증가 람다 주소 : ", callbackUrl)
 
-  // DB에 연결해서 Log를 기록하는 코드를 작성하세요.
-  // init.sql에서 테이블 구성을 참고하세요.
-  // database.js의 recordLog를 참고하세요.
   await req.conn.query(
     recordLog(factory_id, factory_name, item_id, item_name, quantity, requester)
   );
-
+  await req.conn.end();
   console.log("데이터베이스에 생산요청 기록 완료");
 
-  //step 3
-  try {
-    const response = await axios.post(
-      //FILL_ME_IN, FILL_ME_IN
-      callbackUrl,
-      req.body
-    );
-    console.log(response.data);
-  } catch (error) {
-    console.error(error);
+  const now = new Date().toString()
+  const message = `생산 완료! item db에 수량 증가 요청 발송 \n메시지 작성 시각: ${now}`
+  const params = {
+    Message: message,
+    Subject: '수량 증가 요청',
+    MessageAttributes: {
+      MessageAttributeItemId: {
+        StringValue: item_id,
+        DataType: "String",
+      },
+      MessageAttributeItemCnt: {
+        StringValue: `${quantity}`,
+        DataType: "Number",
+      }
+    },
+    TopicArn: process.env.TOPIC_ARN
   }
-  return res.status(200).json({ message_from_factory: `생산 요청 접수!` });
+  console.log("보내는 메시지 결과물  : ", params)
+  await sns.publish(params).promise()
+  return res.status(200).json({ message: `상품 : ${item_id, item_name} \n 수량 : ${quantity}, 데이터베이스 수량 증가 기록 요청중` });
 }
 );
-
-
 
 module.exports.handler = serverless(app);
 
