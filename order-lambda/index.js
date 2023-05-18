482e7ee489c515d052479dc13b0c2275ebc6f6ba
glen15 <glen910115@gmail.com>
2023-05-18 15:10:04 +0900

    update payload in order-lambda

--- order-lambda/index.js
+++ order-lambda/index.js
@@ -7,13 +7,17 @@ const consumer = async (event) => {
     const requester = json.MessageAttributeRequester.Value;
     const quantity = Number(json.MessageAttributeItemCnt.Value);
     const item_id = Number(json.MessageAttributeItemId.Value);
+    const item_name = json.MessageAttributeItemName.Value;
     const factory_id = Number(json.MessageAttributeFactoryId.Value);
+    const factory_name = json.MessageAttributeFactoryName.Value;
 
     const payload = {
       "requester": requester,
       "quantity": quantity,
       "item_id": item_id,
-      "factory_id": factory_id
+      "item_name": item_name,
+      "factory_id": factory_id,
+      "factory_name": factory_name
     }
     console.log(`payload : ${JSON.stringify(payload)}`);
     try {
